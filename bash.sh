5e8fccd995c7c8629f68a9479593d0d450041d73
Ved Ratn Dixit <ved050493@gmail.com>
2014-01-28 14:12:57 +0530

    First Commit

---
+++ bash.sh
@@ -0,0 +1,23 @@
+# !/bin/bash
+
+rm -rf tmp
+mkdir tmp
+
+for file in "test_files"/*.c 
+do
+	file=`echo $file | cut -d '/' -f2`
+	make -f Makefile.cfg FILE=$file
+done
+
+echo "\nProcessing correct files"
+echo "-------------------------\n"
+
+for file in "test_files"/*.cs306.cfg 
+do
+	echo $file
+	f=`echo $file | cut -d '/' -f2`
+	f=`echo $f | cut -d '.' -f1`
+	./cfglp $file -tokens -d > out1
+	./cfglp64-sirki $file -tokens -d > out2
+	diff out1 out2 > tmp/$f
+done
---
+++ test_files/test.c
@@ -0,0 +1,18 @@
+int a;
+main()
+{
+	int b, i;
+	b = a;
+	if(a != b){
+		b = a;
+	}
+	a = b;
+	b = a;
+	i = a;
+	while(a < b){
+		i = i;
+		a = b;
+	}
+
+	
+}
